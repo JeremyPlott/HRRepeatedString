21d9a32f1cf32c53ac22b6e0fac8e094e1da8147
Jeremy <JeremyPlott@gmail.com>
2019-09-30 12:27:19 -0400

    Add project files.

---
+++ HRRepeatedString/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HRRepeatedString {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            // fails test case 5 & 20. Don't know what the input of those is to fix code
+
+            string s = "abcba";
+            s = s.ToLower();
+            int strLen = s.Length;
+            char letter = 'a';
+            long n = 100000000000; 
+            
+            
+            IList<char> charlist = new List<char>();
+
+            foreach (char c in s) {
+                charlist.Add(c);
+            }
+
+            if (strLen == 1)
+            {
+                Console.WriteLine(n);
+            }
+
+            int appearances = charlist.Where(c => c.Equals(letter)).Count();
+            long fullReps = (n / strLen); 
+            long charsRem = (n % strLen);
+
+            List<char> remList = (List<char>)charlist;
+            long counter = 0;
+            long remAppear = 0;
+            while (counter < charsRem) {
+                foreach(char c in charlist) {
+                    if (c.Equals(letter)) {
+                        remAppear++;                        
+                    }
+                    counter++;
+                    if (counter == charsRem) {
+                        break;
+                    }
+                }
+            }            
+
+            long totalAppear = (appearances * fullReps) + remAppear;
+            Console.WriteLine(totalAppear);
+        }
+    }
+}
---
+++ UsingMath/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HRRepeatedString {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            // this attempt fails. Too inefficient
+
+            string s = "abc";
+            s = s.ToLower();
+            string letter = "a";
+            int n = 10;
+            int counter = 0;
+            int target = 0;
+            List<char> charlist = new List<char>();
+
+            foreach (char c in s) {
+                charlist.Add(c);
+            }
+
+            while (counter < n) {
+                foreach (char c in charlist) {
+                    if (c.ToString().Equals(letter)) {
+                        target++;
+                    }
+                    counter++;
+                    if (counter == n + 1) {
+                        break;
+                    }
+                    Console.Write($"{c.ToString()}, {counter}, {target}");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
